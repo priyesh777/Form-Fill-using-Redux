770ede22002eba21cd5f4ff563888a80b1aace5c
priyesh777 <priyeshgautam1010@gmail.com>
2020-04-20 19:18:17 +0545

    some form updates with image issue

--- src/Form.js
+++ src/Form.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 // import { postForm, getPeopleDetails } from './formAction';
 import { setFormData, getPeopleDetailsRequest } from "./actions/form";
-import { Button, Nav, Navbar } from "react-bootstrap";
+import { Figure, Card, Form, Button, Nav, Navbar } from "react-bootstrap";
 
-export const Form = props => {
+const FormComponent = props => {
   const {
     people,
     dispatch,
@@ -35,85 +35,108 @@ export const Form = props => {
         </Nav>
       </Navbar>
       <br /> <br />
-      <div>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              name="firstName"
-              type="text"
-              placeholder="your first Name"
-              value={formData.firstName || " "}
-              onChange={handleChange}
-            />
-          </Form.Group>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt="failed to load img"
+                src="#"
+              />
+              <Figure.Caption>
+                Nulla vitae elit libero, a pharetra augue mollis interdum.
+              </Figure.Caption>
+            </Figure>
+          </div>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              name="lastName"
-              type="text"
-              placeholder="your last Name"
-              value={formData.lastName || " "}
-              onChange={handleChange}
-            />
-          </Form.Group>
+          <div className="col-sm">
+            <Card style={{ width: "25rem" }}>
+              <Card.Body>
+                <Card.Title>Enter your Personal Information </Card.Title>
+                <Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                      name="firstName"
+                      type="text"
+                      placeholder="your first Name"
+                      value={formData.firstName || " "}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              name="email"
-              type="text"
-              placeholder="enter your email"
-              value={formData.email || " "}
-              onChange={handleChange}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                      name="lastName"
+                      type="text"
+                      placeholder="your last Name"
+                      value={formData.lastName || " "}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Age</Form.Label>
-            <Form.Control
-              name="age"
-              type="number"
-              placeholder="First Name"
-              value={formData.age || " "}
-              onChange={handleChange}
-            />
-          </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      name="email"
+                      type="text"
+                      placeholder="enter your email"
+                      value={formData.email || " "}
+                      onChange={handleChange}
+                    />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              name="address"
-              type="text"
-              placeholder="your permanent location"
-              value={formData.address || " "}
-              onChange={handleChange}
-            />
-          </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Age</Form.Label>
+                    <Form.Control
+                      name="age"
+                      type="number"
+                      placeholder="enter your age"
+                      value={formData.age || " "}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Proficiency</Form.Label>
-            <Form.Control
-              name="proficiency"
-              type="text"
-              placeholder="Your proficiency"
-              value={formData.proficiency || " "}
-              onChange={handleChange}
-            />
-          </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                      name="address"
+                      type="text"
+                      placeholder="your permanent location"
+                      value={formData.address || " "}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
 
-          <Button
-            variant="primary"
-            type="submit"
-            onClick={e => handleSubmit(e)}
-          >
-            Submit
-          </Button>
-        </Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Proficiency</Form.Label>
+                    <Form.Control
+                      name="proficiency"
+                      type="text"
+                      placeholder="Your proficiency"
+                      value={formData.proficiency || " "}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={e => handleSubmit(e)}
+                  >
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
       </div>
     </>
   );
@@ -134,4 +157,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Form);
+export default connect(mapStateToProps, mapDispatchToProps)(FormComponent);
--- src/sagas/form.js
+++ src/sagas/form.js
@@ -1,19 +1,17 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { getPeopleDetails } from '../API';
+import { call, put, takeLatest } from "redux-saga/effects";
+import { getPeopleDetails } from "../API";
 import {
   formConstant,
   getPeopleDetailsSuccess,
-  getPeopleDetailsFailure,
-} from '../actions/form';
+  getPeopleDetailsFailure
+} from "../actions/form";
 
 function* getPeopleDetailsRequest(payload) {
   try {
     const response = yield call(getPeopleDetails, payload);
     yield put(getPeopleDetailsSuccess(response.data));
   } catch (error) {
-    yield put(getPeopleDetailsFailure(
-      alert('data fetch error');
-    ));
+    yield put(getPeopleDetailsFailure());
   }
 }
 
